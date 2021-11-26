8060b4037c2089660ed770aa2bdd06d4ac0b6ae9
Renner Borges <rennerferreira23@gmail.com>
2021-11-25 21:02:29 -0300

    feat: init simulador

---
+++ simulacaoSistemaOperacional/src/simulacaosistemaoperacional/Processo.java
@@ -0,0 +1,187 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simulacaosistemaoperacional;
+
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ *
+ * @author renner
+ */
+public class Processo {
+    public static final String PRONTO = "PRONTO";
+    public static final String BLOQUEADO = "BLOQUEADO";
+    public static final String EXECUTANDO = "EXECUTANDO";
+    
+    private String ep = PRONTO;
+    private int pid;
+    private int tp;
+    private int cp;
+    private int nes;
+    private int nCpu;
+    private int qtdCiclos;
+    
+    public void executar(){
+        
+        if(this.tp == this.qtdCiclos){
+            return;
+        }
+        
+        if(this.ep.equals(BLOQUEADO)){
+            
+            if(randomNumber(100) <= 30){
+                this.setEp(PRONTO);
+                return;
+            }else{
+                return;
+            }
+            
+        }
+        
+        setEp(EXECUTANDO);
+
+        for(int i =0; i < 1000; i++){
+           
+            if(this.tp == this.qtdCiclos){
+                return;
+            }
+            
+            if(randomNumber(100) <= 5){
+                setEp(BLOQUEADO);
+                return;
+            }
+            
+            ++this.tp;
+            this.cp = ++this.tp;
+            
+        }
+        setEp(PRONTO);
+
+    }
+    
+    public int randomNumber(int maxNumber){
+        Random random = new Random();
+        return random.nextInt(maxNumber);
+    }
+    
+    public Processo(int pid, int qtdCiclos) {
+        this.pid = pid;
+        this.qtdCiclos = qtdCiclos;
+    }
+    
+    
+    public String getEp() {
+        return ep;
+    }
+
+    public void setEp(String ep) {
+        System.out.println(this.ep + " >>>> " + ep);
+        this.ep = ep;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getTp() {
+        return tp;
+    }
+
+    public void setTp(int tp) {
+        this.tp = tp;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public int getNes() {
+        return nes;
+    }
+
+    public void setNes(int nes) {
+        this.nes = nes;
+    }
+
+    public int getnCpu() {
+        return nCpu;
+    }
+
+    public void setnCpu(int nCpu) {
+        this.nCpu = nCpu;
+    }
+
+    public int getQtdCiclos() {
+        return qtdCiclos;
+    }
+
+    public void setQtdCiclos(int qtdCiclos) {
+        this.qtdCiclos = qtdCiclos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.ep);
+        hash = 59 * hash + this.pid;
+        hash = 59 * hash + this.tp;
+        hash = 59 * hash + this.cp;
+        hash = 59 * hash + this.nes;
+        hash = 59 * hash + this.nCpu;
+        hash = 59 * hash + this.qtdCiclos;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Processo other = (Processo) obj;
+        if (this.pid != other.pid) {
+            return false;
+        }
+        if (this.tp != other.tp) {
+            return false;
+        }
+        if (this.cp != other.cp) {
+            return false;
+        }
+        if (this.nes != other.nes) {
+            return false;
+        }
+        if (this.nCpu != other.nCpu) {
+            return false;
+        }
+        if (this.qtdCiclos != other.qtdCiclos) {
+            return false;
+        }
+        if (!Objects.equals(this.ep, other.ep)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Processo{" + "ep=" + ep + ", pid=" + pid + ", tp=" + tp + ", cp=" + cp + ", nes=" + nes + ", nCpu=" + nCpu + ", qtdCiclos=" + qtdCiclos + '}';
+    }
+
+}
---
+++ simulacaoSistemaOperacional/src/simulacaosistemaoperacional/SimulacaoSistemaOperacional.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simulacaosistemaoperacional;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author renner
+ */
+public class SimulacaoSistemaOperacional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Processo> processos = new ArrayList<>();
+        
+        Processo processo0 = new Processo(0,10000);
+        processos.add(processo0);
+        
+        Processo processo1 = new Processo(1,5000);
+        processos.add(processo1);
+        
+        Processo processo2 = new Processo(2,7000);
+        processos.add(processo2);
+        
+        Processo processo3 = new Processo(3,3000);
+        processos.add(processo3);
+        
+        Processo processo4 = new Processo(4,3000);
+        processos.add(processo4);
+        
+        Processo processo5 = new Processo(5,8000);
+        processos.add(processo5);
+        
+        Processo processo6 = new Processo(6,2000);
+        processos.add(processo6);
+        
+        Processo processo7 = new Processo(7,5000);
+        processos.add(processo7);
+        
+        Processo processo8 = new Processo(8,4000);
+        processos.add(processo8);
+        
+        Processo processo9 = new Processo(9,10000);
+        processos.add(processo9);
+        
+        while(processo8.getTp() != processo8.getQtdCiclos()){
+            processo8.executar();
+            System.out.println(processo8.getTp());
+        }
+        
+    }
+    
+}
